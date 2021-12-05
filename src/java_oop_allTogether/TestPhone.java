084aa714c94e729460df44587f610bdddcc52c34
Sana-Naeem <sananaeemqa@gmail.com>
2021-12-05 18:00:40 -0500

    12052021 6:00pm

--- src/java_oop_allTogether/TestPhone.java
+++ src/java_oop_allTogether/TestPhone.java
@@ -1,11 +1,11 @@
 package java_oop_allTogether;
 
 public class TestPhone {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("\n---------------------Instantiating Iphone1 Class-----------------------------\n");
-		Iphone1 iphone1 = new Iphone1(); //Regular Class Iphone1 class in instantiated here.
+		Iphone1 iphone1 = new Iphone1(); // Regular Class Iphone1 class in instantiated here.
 		iphone1.regularClassInfo();
 		iphone1.abstractClassInfo();
 		iphone1.appleWatchInfo();
@@ -13,13 +13,13 @@ public class TestPhone {
 		iphone1.call();
 		iphone1.message();
 		iphone1.camera();
-		Phone.battery(); //Here the static method is called by the class in which it is present namely Phone Class.
-		iphone1.wireless(); //Here we have called the void method.
-		
-		
-		System.out.println("\n------------------------Instantiating AppleWatch Abstract Class------------------------------\n");
-		
-		AppleWatch appleWatch = new Iphone1(); 
+		Phone.battery(); // Here the static method is called by the class Phone.
+		iphone1.wireless(); // Here we have called the void method.
+
+		System.out.println(
+				"\n------------------------Instantiating AppleWatch Abstract Class------------------------------\n");
+
+		AppleWatch appleWatch = new Iphone1();
 		appleWatch.abstractClassInfo();
 		appleWatch.appleWatchInfo();
 		iphone1.regularClassInfo();
@@ -27,31 +27,31 @@ public class TestPhone {
 		iphone1.call();
 		iphone1.message();
 		iphone1.camera();
-		iphone1.wireless(); //Here we have called the void method.
-		Phone.battery(); //Here the static method is called by the class in which it is present namely Phone Class.
-		
-		
+		iphone1.wireless(); // Here we have called the void method.
+		Phone.battery(); // Here the static method is called by the class Phone.
+
 		System.out.println("\n-------------------------Instantiating Phone Interface-------------------------------\n");
-		
-		Phone phone = new Iphone1(); 
+
+		Phone phone = new Iphone1();
 		phone.interfaceinfo();
 		phone.call();
 		phone.message();
 		phone.camera();
-		phone.wireless(); //Here we have called the void method.
-		Phone.battery(); ////Here the static method is called by the class in which it is present namely Phone Class.
-		
-		
-		System.out.println("\n--------------------Private Variables are called here from Iphone1 Class-----------------------\n");
+		phone.wireless(); // Here we have called the void method.
+		Phone.battery(); //// Here the static method is called by the class in which it is present namely
+							//// Phone Class.
+
+		System.out.println(
+				"\n--------------------Private Variables are called here from Iphone1 Class-----------------------\n");
 
 		iphone1.setInfo("I bought this phone in 2000, the price was: $");
 		iphone1.setPrice(750);
 		iphone1.setUser('F');
 		iphone1.setMadeInUSA(false);
-		System.out.println(iphone1.getInfo() + iphone1.getPrice() + ", user's sex is: " + iphone1.getUser() + ", made in USA: " + 		iphone1.isMadeInUSA());
-		
-		
-		System.out.println("\n----------------------------Instantiating Iphone6 Class----------------------------\n"); 
+		System.out.println(iphone1.getInfo() + iphone1.getPrice() + ", user's sex is: " + iphone1.getUser()
+		+ ", made in USA: " + iphone1.isMadeInUSA());
+
+		System.out.println("\n----------------------------Instantiating Iphone6 Class----------------------------\n");
 		Iphone6 iphone6 = new Iphone6();
 		iphone6.email();
 		iphone6.photos();
@@ -74,11 +74,10 @@ public class TestPhone {
 		iphone6.appleWatchInfo();
 		Phone.battery();
 		iphone1.wireless();
-		
-		
-		System.out.println("\n----------------------------Instantiating Iphone2 Class----------------------------\n"); 
+
+		System.out.println("\n----------------------------Instantiating Iphone2 Class----------------------------\n");
 		Iphone2 iphone2 = new Iphone2();
-		iphone2.userOfIphone2= 'F';
+		iphone2.userOfIphone2 = 'F';
 		iphone2.iPhone2Info();
 		iphone2.iPhone2Info('M');
 		iphone2.dropbox();
@@ -99,32 +98,29 @@ public class TestPhone {
 		iphone2.abstractClassInfo();
 		iphone2.appleWatchInfo();
 		iphone2.wireless();
-		iphone2.storage= 64; 
-		
-	
-		System.out.println("\n-----------------------Executing method overloading-------------------------\n");
+		iphone2.storage = 64;
+
+		System.out.println("\n-----------------------Executing method overloading--------------------------\n");
 		Iphone6 iphone6overloadingIphone6 = new Iphone6();
-		iphone6overloadingIphone6.materials(); //void method
-		iphone6overloadingIphone6.materials(100); //void parameterized method
-		iphone6overloadingIphone6.materials(100,200); //return type parameterized method
-		Iphone6.materials(100,200,300); //return type static method
-		iphone6overloadingIphone6.materials(100,200,300,400); //return type final method
-		
-		
+		iphone6overloadingIphone6.materials(); // void method
+		iphone6overloadingIphone6.materials(100); // void parameterized method
+		iphone6overloadingIphone6.materials(100, 200); // return type parameterized method
+		Iphone6.materials(100, 200, 300); // return type static method
+		iphone6overloadingIphone6.materials(100, 200, 300, 400); // return type final method
+
 		System.out.println("\n------------------------Executing method overriding:---------------------------\n");
 		ConfiguredIphone6 configuredIphone6 = new ConfiguredIphone6();
 		configuredIphone6.materials();
 		configuredIphone6.materials(500);
-		configuredIphone6.materials(200,300);
-		
-		
-		
+		configuredIphone6.materials(200, 300);
+
 		/*
-		 * Comment out at the end of TestPhone to inform me which is hierarchical inheritance, multilevel inheritance, and single 				inheritance.
-		 	As Iphone 2 and Iphone 3 are both subclasses which are inherited by one common parent class so they show a 						Hierarchical inheritance.
-		 	Iphone 4, Iphone 5 and Iphone 6 show single inheritance.
+		 * Comment out at the end of TestPhone to inform me which is hierarchical
+		 * inheritance, multilevel inheritance, and single inheritance. As Iphone 2 and
+		 * Iphone 3 are both subclasses which are inherited by one common parent class
+		 * so they show a Hierarchical inheritance. Iphone 4, Iphone 5 and Iphone 6 show
+		 * single inheritance.
 		 */
-		
-		
+
 	}
 }
